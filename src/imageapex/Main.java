0273c117b5c9cb5267adf28033414462904b52a1
陈志超 <youtiao@stu.scau.edu.cn>
2024-05-10 11:13:05 +0800

    first
    可运行版本

---
+++ src/imageapex/Main.java
@@ -0,0 +1,44 @@
+package imageapex;
+
+import com.jfoenix.controls.JFXDecorator;
+
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("OnlyViewer");
+
+        //根据屏幕大小自适应设置长宽
+        double width = 800;
+        double height = 600;
+        try {
+            Rectangle2D bounds = Screen.getScreens().get(0).getBounds();
+            width = bounds.getWidth() / 1.45;
+            height = bounds.getHeight() / 1.35;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        Parent root = FXMLLoader.load(getClass().getResource("/imageapex/main/resources/fxml/Home.fxml"));
+        Scene scene = new Scene(new JFXDecorator(primaryStage, root), width, height);
+
+        primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("/imageapex/main/resources/icons/app.png")));
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        System.out.println("Starting Only Viewer...");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
